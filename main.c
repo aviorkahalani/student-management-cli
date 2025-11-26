8895cc2ac3d95d49f8ca9001630e6c4a823d726e
Avior Kahalani <aviorkahalani@gmail.com>
2025-11-26 02:26:37 +0200

    feat: add helper for readin input

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// macros
+#define BUFFER_SIZE 1024
 
 // functions declaration
 void print_menu(void);
+int read_option(void);
+bool is_valid_option(int option);
 
 int main(void)
 {
@@ -20,4 +27,32 @@ void print_menu(void)
       "\t6. Save data to file\n"
       "\t7. Load data from file\n"
       "Your input: ");
+}
+
+int read_option(void)
+{
+  char option_input[BUFFER_SIZE];
+  while (true)
+  {
+    if (!fgets(option_input, BUFFER_SIZE, stdin))
+    {
+      printf("Input error. Please try again: ");
+      continue;
+    }
+
+    int option = strtol(option_input, NULL, 10);
+
+    if (!is_valid_option(option))
+    {
+      printf("Invalid option. Please try again: ");
+      continue;
+    }
+
+    return option;
+  }
+}
+
+bool is_valid_option(int option)
+{
+  return option >= 1 && option <= 7;
 }

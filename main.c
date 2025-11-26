f95153d1fc245f3414ecaa4703697222658a5db1
Avior Kahalani <aviorkahalani@gmail.com>
2025-11-26 02:17:27 +0200

    feat: print options menu

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+// functions declaration
+void print_menu(void);
+
 int main(void)
 {
-  printf("Hello, World!\n");
+  print_menu();
+}
+
+void print_menu(void)
+{
+  printf(
+      "Choose an option (1 - 7):\n"
+      "\t1. Add student\n"
+      "\t2. List students\n"
+      "\t3. Search students\n"
+      "\t4. Update student\n"
+      "\t5. Remove student\n"
+      "\t6. Save data to file\n"
+      "\t7. Load data from file\n"
+      "Your input: ");
 }
